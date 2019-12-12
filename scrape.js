20ac1abc4af0ddb2c9c11877d280c739c2109e64
Jon Barclay <my@nameisjon.com>
2019-12-12 11:32:37 -0600

    returns array of routes

--- scrape.js
+++ scrape.js
@@ -1,32 +1,34 @@
 // imports
 require('dotenv').config();
 const axios = require('axios');
-
-// url to scrape
-const url = process.env.API_URL;
+const cheerio = require('cheerio');
 
 // auth
-const bearerToken = process.env.BEARER_TOKEN;
-axios.defaults.headers.common = { 'Authorization': `Bearer ${bearerToken}` }
+axios.defaults.headers.common = { 'Authorization': `Bearer ${process.env.BEARER_TOKEN}` }
+
+const rows = [];
 
 // fetch
 const fetchData = async () => {
     try {
-        const result = await axios.get(url);
-        return cheerio.load(result.data);
+        await axios.get(process.env.API_URL);
     } catch (err) {
-        console.log(err);
+        // console.log(err.response.data);
+        return cheerio.load(err.response.data);
     }
 }
 
 const getResults = async () => {
     try {
         const $ = await fetchData();
-        const routes = $('.route_table');
-        console.log(routes);
+        $('.route_row').each(function(i, elem) {
+            rows[i] = $(':nth-child(3)', this).text();
+        });
+        // rows.forEach(item => console.log(item));
+        return rows;
     } catch (err) {
         console.log(err);
     }
 }
 
-getResults();
+module.exports = getResults;
